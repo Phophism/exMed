61e7729ca61a3c04323e8b8452a81e302144d254
akkapop prasompon <akkapop_mar@hotmail.com>
2021-02-12 22:39:05 +0700

    Update login.php

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -8,6 +8,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>assets/images/logo/CMEx.png">
 
   <title><?php echo $title; ?></title>
   <!-- Favicon icon -->
